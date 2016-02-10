2e2ed1eec4df8b5a615ab9d8f992483f9daedc24
jingyuw <wjy920421@yahoo.com>
2016-02-09 22:09:22 -0600

    broken code...

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -29,3 +29,74 @@ class Interpreter {
 class EvalException extends RuntimeException {
     EvalException(String msg) { super(msg); }
 }
+
+
+
+class InterpreterVisitor implements ASTVisitor<JamVal> {
+
+    @Override
+    public JamVal forBoolConstant(BoolConstant b) {
+        return b;
+    }
+
+    @Override
+    public JamVal forIntConstant(IntConstant i) {
+        return i;
+    }
+
+    @Override
+    public JamVal forNullConstant(NullConstant n) {
+        return JamEmpty.ONLY;
+    }
+
+    @Override
+    public JamVal forVariable(Variable v) {
+        // TODO Auto-generated method stub
+        return new Cons<AST>();
+    }
+
+    @Override
+    public JamVal forPrimFun(PrimFun f) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forUnOpApp(UnOpApp u) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forBinOpApp(BinOpApp b) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forApp(App a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forMap(Map m) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forIf(If i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JamVal forLet(Let l) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
+
+
