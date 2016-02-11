82b86d3d723472f17d8e1a6305aedc8386c3083a
jingyuw <wjy920421@yahoo.com>
2016-02-11 03:10:03 -0600

    combine test cases of assignment 1

--- src/Assign1Test.java
+++ src/Assign1Test.java
@@ -101,5 +101,201 @@ public class Assign1Test extends TestCase {
   } //end of func
     
 
+  
+  
+  
+  public void testParseExp_Term() {
+      try {
+          String output = "true";
+          String input = " true ";
+          checkString("testParseExp_Term", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseExp_Term threw " + e);
+      }
+  } 
+  
+  public void testParseExp_TermBinopExp() {
+      try {
+          String output = "(a(b) + x(y))";
+          String input = "(a)(b)+(x)(y)";
+          checkString("testParseExp_TermBinopExp", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseExp_TermBinopExp threw " + e);
+      }
+  } 
+  
+  public void testParseExp_If() {
+      try {
+          String output = "if - x then y else z";
+          String input = "if -x then   y else z";
+          checkString("testParseExp_If", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseExp_If threw " + e);
+      }
+  } 
+  
+  public void testParseExp_Let() {
+      try {
+          String output = "let y := function?(); in number?";
+          String input = "let y:=function?(); in number?";
+          checkString("testParseExp_Let", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseExp_Let threw " + e);
+      }
+  } 
+  
+  public void testParseExp_Map() {
+      try {
+          String output = "map x,y,z to a";
+          String input = "map x, y, z to a";
+          checkString("testParseExp_Map", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseExp_Map threw " + e);
+      }
+  } 
+  
+  public void testParseTerm_UnopTerm() {
+      try {
+          String output = "- function?(f())";
+          String input = "-function?(f())";
+          checkString("testParseTerm_UnopTerm", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseTerm_UnopTerm threw " + e);
+      }
+  } 
+  
+  public void testParseTerm_Factor() {
+      try {
+          String output = "a(b)";
+          String input = "(a)(b)";
+          checkString("testParseTerm_Factor", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseTerm_Factor threw " + e);
+      }
+  } 
+  
+  public void testParseTerm_FactorExpList() {
+      try {
+          String output = "(if x then y else z)(a, b0, _c?)";
+          String input = "(if x then y else z) (a,b0,_c?)";
+          checkString("testParseTerm_FactorExpList", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseTerm_FactorExpList threw " + e);
+      }
+  } 
+  
+  public void testParseTerm_Constant() {
+      try {
+          String output = "null";
+          String input = "null ";
+          checkString("testParseTerm_Constant", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseTerm_Constant threw " + e);
+      }
+  } 
+  
+  public void testParseFactor_Exp() {
+      try {
+          String output = "if x then y else z";
+          String input = "if x then y else z";
+          checkString("testParseFactor_Exp", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseFactor_Exp threw " + e);
+      }
+  } 
+  
+  public void testParseFactor_Prim() {
+      try {
+          String output = "function?";
+          String input = "function?";
+          checkString("testParseFactor_Prim", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseFactor_Prim threw " + e);
+      }
+  } 
+  
+  public void testParseFactor_Id() {
+      try {
+          String output = "_x0";
+          String input = "_x0";
+          checkString("testParseFactor_Id", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseFactor_Id threw " + e);
+      }
+  }
+  
+  public void testParseException_1() {
+      try {
+          String output = "fail";
+          String input = "x := y";
+          checkString("testParseException_1", output, input );
+          fail("parseException did not throw ParseException exception");
+      }
+      catch (ParseException e) {
+      }
+      catch (Exception e) {
+          fail("testParseException_1 threw " + e);
+      }
+  }
+  
+  public void testParseException_2() {
+      try {
+          String output = "fail";
+          String input = "if a then b else;";
+          checkString("testParseException_2", output, input );
+          fail("parseException did not throw ParseException exception");
+      }
+      catch (ParseException e) {
+      }
+      catch (Exception e) {
+          fail("testParseException_2 threw " + e);
+      }
+  }
+  
+  public void testParseException_3() {
+      try {
+          String output = "fail";
+          String input = "map x,y, to z";
+          checkString("testParseException_3", output, input );
+          fail("parseException did not throw ParseException exception");
+      }
+      catch (ParseException e) {
+      }
+      catch (Exception e) {
+          fail("testParseException_3 threw " + e);
+      }
+  }
+  
+  public void testParseHard() {
+      try {
+          String output = "let f := map n to if (n = 0) then 1 else (n * f((n - 1))); "
+                        + "in let f := map n,m,k to if ((((n <= 0) & n) >= 0) | "
+                        + "(((((n < 0) & n) > 0) & n) != 0)) then number? else (m / f((k + 1))); "
+                        + "in let x := 3; y := 4; z := cons?(function?((x * ~ y)), cons(- arity(x))); "
+                        + "in let x := 3; y := 4; z := g(); in (g(x, y, z))(null?(true), list?(false), first(null))";
+          String input = "let \r\n f := map n to if n = 0 then 1 else n * f(n - 1); "
+                       + "\r\n in \r\n let \r\n f := map n,m,k to if (n <= 0 & n >= 0)"
+                       + "\r\n | (n < 0 & n > 0 & n != 0) then number?\r\n else m / f(k + 1);"
+                       + "\r\n in \r\n  let x:=3;\r\n   y:=4;\r\n  z:=cons?(function?(x * ~y),"
+                       + " cons(-arity(x)));\r\n  in \r\n let x:=3;\r\n y:=4;\r\n  z:=g();\r\n"
+                       + "  in \r\n (g(x,y,z))(null?(true),list?(false),first(null))";
+          checkString("testParseException_3", output, input );
+      }
+      catch (Exception e) {
+          fail("testParseException_3 threw " + e);
+      }
+  }
 
 }
